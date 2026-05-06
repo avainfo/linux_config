2d25dda82bae8b75041aeae2b25476f31f631c9b
avainfo <avainfoconseils@gmail.com>
2026-05-06 04:06:31 +0100

    nvim(plugins): add way to use surround plugin with leader

--- dotfiles/nvim/lua/plugins/surround.lua
+++ dotfiles/nvim/lua/plugins/surround.lua
@@ -4,5 +4,50 @@ return {
 	event = "VeryLazy",
 	config = function()
 	require("nvim-surround").setup({})
+
+	local map = vim.keymap.set
+	local opts = { remap = true, silent = true }
+
+	-- Normal mode: wrap current word
+	map("n", '<leader>a"', 'ysiw"', vim.tbl_extend("force", opts, {
+		desc = "Wrap word with double quotes",
+	}))
+
+	map("n", "<leader>a'", "ysiw'", vim.tbl_extend("force", opts, {
+		desc = "Wrap word with single quotes",
+	}))
+
+	map("n", "<leader>ap", "ysiw)", vim.tbl_extend("force", opts, {
+		desc = "Wrap word with parentheses",
+	}))
+
+	map("n", "<leader>ab", "ysiw]", vim.tbl_extend("force", opts, {
+		desc = "Wrap word with brackets",
+	}))
+
+	map("n", "<leader>aB", "ysiw}", vim.tbl_extend("force", opts, {
+		desc = "Wrap word with braces",
+	}))
+
+	-- Visual mode: wrap selected text
+	map("x", '<leader>a"', 'S"', vim.tbl_extend("force", opts, {
+		desc = "Wrap selection with double quotes",
+	}))
+
+	map("x", "<leader>a'", "S'", vim.tbl_extend("force", opts, {
+		desc = "Wrap selection with single quotes",
+	}))
+
+	map("x", "<leader>ap", "S)", vim.tbl_extend("force", opts, {
+		desc = "Wrap selection with parentheses",
+	}))
+
+	map("x", "<leader>ab", "S]", vim.tbl_extend("force", opts, {
+		desc = "Wrap selection with brackets",
+	}))
+
+	map("x", "<leader>aB", "S}", vim.tbl_extend("force", opts, {
+		desc = "Wrap selection with braces",
+	}))
 	end,
 }
