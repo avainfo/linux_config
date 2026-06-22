4dde883252f1436b8fb638785a2485ed5b20dfea
avainfo <avainfoconseils@gmail.com>
2026-06-22 02:20:06 +0100

    chore(nvim): configure local autoindent and override editorconfig
    Enable local autoindentation and explicitly disable the editorconfig
    integration for the buffer. This ensures that the custom indentation
    rules defined in the autocommand are strictly applied and not
    overwritten by project-level editorconfig files.

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -90,7 +90,10 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.opt_local.softtabstop = 4
 		vim.opt_local.shiftwidth = 4
 		vim.opt_local.cindent = false
+		vim.opt_local.autoindent = true
 		vim.opt_local.smartindent = true
+
+		vim.b.editorconfig = false
 	end,
 })
 
